675a39d5581e36f352e76d62e7cc52689ef05491
Patrick WANG <ruochwang@gmail.com>
2019-07-09 01:38:25 +0800

    add setfilter api and related functions

--- src/lb_networking/libpcap/pcap.c
+++ src/lb_networking/libpcap/pcap.c
@@ -1,4 +1,5 @@
 #include "pcap.h"
+#include <stdlib.h>
 #include "../../lb_core/enclave/etap_t.h"
 
 const uint8_t * pcap_next(pcap_t * handle, struct pcap_pkthdr * header)
@@ -28,17 +29,23 @@ int pcap_loop(pcap_t *p, int cnt, pcap_handler callback, u_char *user)
 		// We only support live capture instead of reading saved file option
 		// We also do not accept -1, 0 value for cnt to represent infinity
 		// pcap_loop may ignore timeout as stated offically. 
+		// TODO pktData is type of uint8_t, need to change to u_char perhaps?
 		static uint8_t pktData[8192];
 		struct pcap_pkthdr * pktHeader;
 		for (int i = 0; i < cnt; i++){
 				read_pkt(pktData, &pktHeader->len, (time_t*)&pktHeader->ts);
 				pktHeader->caplen = pktHeader->len;
+				// Run the packet filter before applying callback function
+				if (bpf_filter(p->fcode.bf_insns,pktData, pktHeader->len,
+										pktHeader->caplen) == 0) {
+						continue;
+				}
 				callback(NULL, pktHeader, pktData);
 		}
 		return 0;
 }
 
-
+/*
 int pcap_compile(pcap_t *p, struct bpf_program *program, const char *buf,
 				int optimize, bpf_u_int32 mask)
 {
@@ -59,6 +66,34 @@ int pcap_compile(pcap_t *p, struct bpf_program *program, const char *buf,
 		
 }
 
+*/
+
+int pcap_setfilter(pcap_t * handle, struct bpf_program *filter) 
+{
+		if (!handle) return -1;
+		if (!filter) return -2;
+		// install the bpf program (filter) to handle
+		// TODO handle now is a pure int, need to change to a singleton later
+		// if (install_bfp_program(handle, filter) < 0) return -3;
+		
+		size_t prog_size;
+
+		// TODO validate the filter program 
+		
+		// TODO free up any already installed program
+		// Do it after refactored the handle struct (pcap_t)
+		
+		prog_size = sizeof(*filter->bf_insns) * filter->bf_len;
+		handle->fcode.bf_len = filter->bf_len;
+		handle->fcode.bf_insns = (struct bfp_insn *)malloc(prog_size);
+		if (handle->fcode.bf_insns == NULL) {
+				// Error occurred
+				return -3;
+		}
+		memcpy(handle->fcode.bf_insns, filter->bf_insns, prog_size);
+		return 0;
+}
+
 int pcap_inject(pcap_t * handle, const void * data, unsigned long len)
 {
 	// TODO: send one packet
@@ -72,7 +107,7 @@ int pcap_set_buffer_size(pcap_t * handle, int size)
 
 void pcap_close(pcap_t * handle)
 {
-	//nothing to do
+	pcap_freecode(handle->fcode);
 }
 
 pcap_t * pcap_create(const char * dev_name, char * error_buffer)
@@ -81,10 +116,24 @@ pcap_t * pcap_create(const char * dev_name, char * error_buffer)
 #else
 	ocall_pcap_init((char*)dev_name);
 #endif
-	return (pcap_t*)1;
+	// return (pcap_t*)1;
+	pcap_t * handle;
+	handle = struct pcap_t p;
+	return handle;
 }
 
 int pcap_activate(pcap_t * handle)
 {
 	return 0;
 }
+
+// Clean up a "struct bpf_program" by freeing all the memory allocated in it.
+void pcap_freecode(struct bpf_program *program) 
+{
+	program->bf_len = 0;
+	if (program->bf_insns != NULL) {
+			free ((char *)program->bf_insns);
+			program->bf_insns = NULL;
+	}
+}
+
--- src/lb_networking/libpcap/pcap.h
+++ src/lb_networking/libpcap/pcap.h
@@ -3,7 +3,7 @@
 
 #include "stdint.h"
 #include <sys/types.h>	/* u_int, u_char etc. */
-
+#include "bpf.h"
 //#include "time.h"
 // You need use your time port instead of this.
 #ifdef PCAP_SELF_TIME
@@ -20,16 +20,36 @@ struct timeval
 #endif
 
 // pcap.h
-typedef int pcap_t;
+// typedef int pcap_t;
 typedef void (*pcap_handler)(u_char *, const struct pcap_pkthdr*, const u_char *);
+typedef u_int bpf_u_int32;
 
 #define PCAP_ERRBUF_SIZE 256
+
+struct pcap_t {
+	struct bpf_program fcode;
+};
 
 struct pcap_pkthdr {
 	struct timeval ts;	/* time stamp */
 	uint32_t caplen;	/* length of portion present */
 	uint32_t len;	/* length this packet (off wire) */
 };
+
+// TODO: might include a bpf.h header file later
+struct bpf_program {
+	u_int bf_len;
+	struct bpf_insn *bf_insns;
+};
+
+// The instruction data structure
+struct bpf_insn {
+	u_short code;
+	u_char jt;
+	u_char jf;
+	bpf_u_int32 k;
+};
+
 
 const uint8_t *pcap_next(pcap_t * handle, struct pcap_pkthdr *header);
 int	pcap_inject(pcap_t *handle, const void * data, unsigned long len);
@@ -43,6 +63,7 @@ int	pcap_activate(pcap_t * handle);
 int pcap_loop(pcap_t *, int, pcap_handler, u_char *);
 int pcap_compile(pcap_t *, struct bpf_program *, const char *, int, bpf_u_int32);
 
+u_int bpf_filter(const struct bpf_insn *pc, const u_char *p, u_int wirelen, u_int buflen);
 
 
 #endif // !SGX_PCAP_H
