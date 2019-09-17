65af5a41118c0afad8027e7841112c71b2da79ca
Patrick WANG <ruochwang@gmail.com>
2019-09-17 13:11:15 +0800

    libntoh module refactored

--- src/lb_networking/libntoh/sfhash.c
+++ src/lb_networking/libntoh/sfhash.c
@@ -1,50 +1,50 @@
 #include "sfhash.h"
 #include "libntoh.h"
 
-#define get16bits(d) (*((const unsigned short *) (d)))
+#define get16bits(d) (*((const unsigned short *)(d)))
 
 /* The most generic version, hashes an arbitrary sequence
  * of bytes.  No alignment or length assumptions are made about
  * the input key.
  */
-_HIDDEN unsigned int sfhash(const void * key, unsigned int len, unsigned int initval)
-{
-	const char	*data = key;
-	unsigned int	hash = len + initval, tmp;
-	int		rem;
+_HIDDEN unsigned int sfhash(const void *key, unsigned int len,
+			    unsigned int initval) {
+	const char *data = key;
+	unsigned int hash = len + initval, tmp;
+	int rem;
 
-	if (len <= 0 || !data )
-		return 0;
+	if (len <= 0 || !data) return 0;
 
 	rem = len & 3;
 	len >>= 2;
 
 	/* Main loop */
-	for (; len > 0; len--)
-	{
+	for (; len > 0; len--) {
 		/* Mix 32bit chunk of the data */
 		hash += get16bits(data);
-		tmp   = (get16bits(data+2) << 11) ^ hash;
-		hash  = (hash << 16) ^ tmp;
-		data += 2*sizeof(unsigned short);
+		tmp = (get16bits(data + 2) << 11) ^ hash;
+		hash = (hash << 16) ^ tmp;
+		data += 2 * sizeof(unsigned short);
 		hash += hash >> 11;
 	}
 
 	/* Handle end cases */
-	switch (rem)
-	{
-		case 3:	hash += *((unsigned short *)data);
+	switch (rem) {
+		case 3:
+			hash += *((unsigned short *)data);
 			hash ^= hash << 16;
 			hash ^= data[sizeof(unsigned short)] << 18;
 			hash += hash >> 11;
 			break;
 
-		case 2:	hash += *((unsigned short *)data);
+		case 2:
+			hash += *((unsigned short *)data);
 			hash ^= hash << 11;
 			hash += hash >> 17;
 			break;
 
-		case 1: hash += *data;
+		case 1:
+			hash += *data;
 			hash ^= hash << 10;
 			hash += hash >> 1;
 			break;
